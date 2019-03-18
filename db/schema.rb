dd995687a0489513152b0a8373939d09c7994527
Stephen Falck <stephenvfalck@gmail.com>
2019-03-18 11:40:36 +0000

    added name comlumn to users table and included name attribute to sign_up_params and account_update_params in registration controller

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_102050) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
